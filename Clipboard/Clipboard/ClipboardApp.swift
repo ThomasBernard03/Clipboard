665b8cc4cbc5fa8537a1d47898991432fab4b830
Thomas <thomas.bernard@diiage.org>
2022-11-29 17:17:50 +0100

    Project init

---
+++ Clipboard/Clipboard/ClipboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardApp.swift
+//  Clipboard
+//
+//  Created by Thomas Bernard on 29/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct ClipboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
